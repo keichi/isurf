df78afb98bbafa36f26d2445183ff7f203b735ce
Keichi Takahashi <keichi.t@me.com>
2013-02-01 00:38:29 +0900

    入出力周りのコードを整理

--- IsosurfaceGenerator/Main.cs
+++ IsosurfaceGenerator/Main.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
 
 using IsosurfaceGenerator.Utils;
 using IsosurfaceGenerator.Exporter;
@@ -12,20 +14,13 @@ namespace IsosurfaceGenerator
 	{
 		public static void Main (string[] args)
 		{
-			Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
-
+			initializeProgram();
 			printCopyrights();
 
-			if (args.Length < 1 || String.IsNullOrWhiteSpace(args[0])) {
-				Console.WriteLine("Please specify a GrADS CTL file.");
-				return;
-			}
-			if (args.Length < 2 || String.IsNullOrWhiteSpace(args[1])) {
-				Console.WriteLine("Please specify the value you want generate the isosurface.");
-				return;
-			}
-			var ctlFilename = args[0];
-			var isoValue = float.Parse(args[1]);
+			var ctlFilename = "";
+			var isoValue = 0.0f;
+
+			parseCommandLineArgs(args, ref ctlFilename, ref isoValue);
 
 			if (File.Exists(ctlFilename)) {
 				processSingleFile(ctlFilename, isoValue);
@@ -33,12 +28,21 @@ namespace IsosurfaceGenerator
 				var files = Directory.GetFiles(ctlFilename, "*.ctl");
 				foreach (var file in files) {
 					processSingleFile(file, isoValue);
-					GC.Collect();
 				}
 			}
 		}
 
-		private static void processSingleFile(string filename, float isoValue) {
+		private static void initializeProgram()
+		{
+			Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+			AppDomain.CurrentDomain.UnhandledException += (object sender, UnhandledExceptionEventArgs e) => {
+				Debug.WriteLine(e.ExceptionObject.ToString());
+				Environment.Exit(-1);
+			};
+		}
+
+		private static void processSingleFile(string filename, float isoValue)
+		{
 			Console.WriteLine ("====================");
 			Console.WriteLine ("Start processing CTL file \"{0}\"", filename);
 
@@ -47,7 +51,22 @@ namespace IsosurfaceGenerator
 			processor = null;
 		}
 
-		private static void printCopyrights() {
+		private static void parseCommandLineArgs(string[] args, ref string ctlFilename, ref float isoValue)
+		{
+			if (args.Length < 1 || String.IsNullOrWhiteSpace(args[0])) {
+				Console.WriteLine("Please specify a GrADS CTL file.");
+				return;
+			}
+			if (args.Length < 2 || String.IsNullOrWhiteSpace(args[1])) {
+				Console.WriteLine("Please specify the value you want generate the isosurface.");
+				return;
+			}
+			ctlFilename = args[0];
+			isoValue = float.Parse(args[1]);
+		}
+
+		private static void printCopyrights()
+		{
 			Console.WriteLine("Isosurface Generator {0}\n{1}\n",
 			                  Assembly.GetExecutingAssembly().GetName().Version,
 			                  ((AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyCopyrightAttribute))).Copyright
